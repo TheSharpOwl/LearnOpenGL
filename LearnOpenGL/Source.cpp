04bc3b365f34ddc9fd974f83b3bfc272fd8ff930
Mohamad Ziad Alkabakibi <ziadxkabakibi@hotmail.com>
2019-07-04 00:34:55 +0300

    HelloWindow done

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -4,22 +4,71 @@
 
 #include<iostream>
 
-int main()
+void initGLFW()
 {
-	//make sure that we're using version 3 fir both minor and major
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//if we press the escape key set the 'should close' to true so the window will close
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//if it's not pressed, it returns GLFW_RELEASE
+		glfwSetWindowShouldClose(window, true);
+}
+int main()
+{
+	
+	//make sure that we're using version 3 fir both minor and major
+	initGLFW();
 
 	//Generate the Window Object
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	glfwMakeContextCurrent(window);//we should make this directly after creating the window
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
+	//initialize GLAD before any OpenGL call but 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	
+	glViewport(0, 0, 800, 600);//parameters : x,y for the lower left corner of the window, last 2 are width and height
+
+	/*
+	*Note that processed coordinates in OpenGL are between -1 and 1 
+	*so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600). 
+	*/
+
+	//register so the function so that it gets called everytime we resize the window
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//simple 'render loop' which keeps our window open until the user closes it and we put the rendering commands inside it.
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);//definition is up there =)
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();//checks if any events are triggered like input
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+	}
+
+	//properly clean/delete all of GLFW's resources that were allocated
+	glfwTerminate();
+
 	return 0;
 }
