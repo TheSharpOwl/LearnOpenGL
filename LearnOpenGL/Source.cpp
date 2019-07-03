9aef38b0a19861ab654b65e0ce435e27c38bcd6f
Mohamad Ziad Alkabakibi <ziadxkabakibi@hotmail.com>
2019-07-03 22:55:42 +0300

    finished half of HelloWindow

---
+++ LearnOpenGL/Source.cpp
@@ -0,0 +1,25 @@
+//always make sure to include glad before glfw
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include<iostream>
+
+int main()
+{
+	//make sure that we're using version 3 fir both minor and major
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Generate the Window Object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	return 0;
+}
