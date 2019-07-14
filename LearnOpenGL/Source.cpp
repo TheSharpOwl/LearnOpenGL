8b370f01ddcd51f747709d427aa5c7ba3e99e32e
Mohamad Ziad Alkabakibi <ziadxkabakibi@hotmail.com>
2019-07-14 07:23:14 +0300

    Drawing a Rectangle after adding an index buffer

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -145,21 +145,35 @@ int main()
 	 when calling glVertexAttribPointer. Since the previously defined VBO is still bound
 	before calling glVertexAttribPointer vertex attribute 0 is now associated with its vertex data.
 	*/
-	unsigned int VAO, VBO;
+	unsigned int VAO, VBO, EBO;
 	float Vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		 -0.5f, -0.5f, 0.0f,
+		 -0.5f, 0.5f, 0.0f
+	};
+	unsigned int Indices[] =
+	{
+		0, 1, 3, //first triangle
+		1, 2, 3	 //second triangle
 	};
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	//0 because layout (location = 0) in the shader
+	//copy index array to an element buffer to be used by OpenGL
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -171,11 +185,16 @@ int main()
 
 		glUseProgram(ShaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glfwTerminate();
 	return 0;
 }
