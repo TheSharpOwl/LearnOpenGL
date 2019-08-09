eecdc521babf6e976ba986136a17346f102e77d5
silver7k <m.ziad.k2000@gmail.com>
2019-08-09 19:37:15 +0300

    multiple boxes now, Coordinate systems is done without the exercises

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -189,6 +189,20 @@ int main()
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
+	glm::vec3 CubePositions[] = 
+	{
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	unsigned int Indices[] =
 	{
 		0, 1, 3,
@@ -247,8 +261,7 @@ int main()
 
 		//do a transformation
 		//Coordinate system :
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, float(glfwGetTime()), glm::vec3(0.5f, 1.f, 0.f));
+		
 
 		glm::mat4 view = glm::mat4(1.f);
 		//transalting the scene in the opposite direction of where we wanna move
@@ -262,8 +275,8 @@ int main()
 		Trans = glm::rotate(Trans, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f));
 
 		OurShader.SetMat4("transform", Trans);
-		int ModelLoc = glGetUniformLocation(OurShader.ID, "model");
-		glUniformMatrix4fv(ModelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		//int ModelLoc = glGetUniformLocation(OurShader.ID, "model");
+		//glUniformMatrix4fv(ModelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 		int ViewLoc = glGetUniformLocation(OurShader.ID, "view");
 		glUniformMatrix4fv(ViewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -273,8 +286,16 @@ int main()
 
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, CubePositions[i]);
+			float angle = 20.f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.f, 0.3f, 0.5f));
+			OurShader.SetMat4("model", model);
 
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		//TEST
 		// second transformation
